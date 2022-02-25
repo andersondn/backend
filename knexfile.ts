eadc202d84794305cc6f9a4594e5ff9bc7e48018
Anderson Nunes <andersondngto@gmail.com>
2022-02-25 20:53:17 -0300

    config: init knex file

---
+++ knexfile.ts
@@ -0,0 +1,38 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/db.sqlite3"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    }
+  },
+
+  staging: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/db.sqlite3"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    }
+  },
+
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/db.sqlite3"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    }
+  }
+
+};
+
+module.exports = config;
