44cfcb0ee7b3a1c9d61b8f9008e02c71ead3ce54
Anderson Nunes <andersondngto@gmail.com>
2022-02-26 12:17:18 -0300

    fear: create route to list user

--- src/routes/user.Routes.ts
+++ src/routes/user.Routes.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import CreateUserController from "../modules/user/useCase/createUserController";
+import ListUserController from "../modules/user/useCase/listUserController";
 
 const routes = Router();
+const listUserController = new ListUserController();
 const createUserController = new CreateUserController();
+
+routes.get(
+   '/',
+    listUserController.handler
+)
 
 routes.post(
     '/',
     createUserController.validate,
     createUserController.handler
-     );
+);
+
 
 export default routes;
