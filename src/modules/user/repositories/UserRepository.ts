34ac31d8e77734196b7aca207a8cf157924ca24b
Anderson Nunes <andersondngto@gmail.com>
2022-02-26 13:50:18 -0300

    feat: create get user repository

--- src/modules/user/repositories/UserRepository.ts
+++ src/modules/user/repositories/UserRepository.ts
@@ -3,6 +3,7 @@ import User from "../entities/User";
 interface UserRepository {
     insertUser(user: Omit<User, 'id' | 'created_at' | 'updated_at'>): Promise<User>;
     getUserByEmail(email: string): Promise<User>;
+    getUserById(userId: number): Promise<User>;
     listUsers(): Promise<User[]>;
 }
 export default UserRepository;
