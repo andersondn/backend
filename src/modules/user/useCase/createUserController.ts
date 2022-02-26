b9dfa67e0bf88c11dcfeda2d51a3819687eee0a9
Anderson Nunes <andersondngto@gmail.com>
2022-02-26 10:41:00 -0300

    fix: user creation validation

--- src/modules/user/useCase/createUserController.ts
+++ src/modules/user/useCase/createUserController.ts
@@ -1,24 +1,32 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import CreateUserUseCase from "./createUserUseCase";
-import { Joi, Segments, validation } from "../../../shared/infra/http/express/middlewares/Celebrate";
+import {
+  Joi,
+  Segments,
+  validation,
+} from "../../../shared/infra/http/express/middleware/Celebrate";
 class CreateUserController {
-  validate = validation(
-    {
-      [Segments.BODY]: Joi.object().keys({
-        name: Joi.string().required(),
-        password: Joi.string().required(),
-        email: Joi.string().required(),
-        departmentId: Joi.number(),
-        role: Joi.string().default("EMPLOYEE"),
-      }),
-    },
- 
-  );
+  validate = validation({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      password: Joi.string().required(),
+      email: Joi.string().required(),
+      departmentId: Joi.number(),
+      role: Joi.string().default("EMPLOYEE"),
+    }),
+  });
 
   async handler(request: Request, response: Response) {
+    const { name, email, password, departmentId, role } = request.body;
     const createUserUseCase = container.resolve(CreateUserUseCase);
-    const result = await createUserUseCase.execute();
+    const result = await createUserUseCase.execute({
+      name,
+      email,
+      password,
+      departmentId,
+      role,
+    });
     return response.json(result);
   }
 }
--- src/modules/user/useCase/createUserUseCase.ts
+++ src/modules/user/useCase/createUserUseCase.ts
@@ -1,22 +1,38 @@
 import { inject, injectable } from "tsyringe";
+import User from "../entities/User";
 import UserRepository from "../repositories/UserRepository";
+
+type CreateUserParams = {
+  name: string;
+  email: string;
+  password: string;
+  departmentId: number;
+  role: "ADMIN" | "MANAGER" | "EMPLOYEE";
+};
 
 @injectable()
 class CreateUserUseCase {
-    constructor (
-        @inject('UserRepository')
-        private userRepository: UserRepository
-    ) {}
-    async execute(){
-        const user = await this.userRepository.insertUser({
-            name: 'John Doe',
-            email: 'aaa',
-            password: '',
-            role: 'ADMIN',
-            department_id: 1
-        });
+  constructor(
+    @inject("UserRepository")
+    private userRepository: UserRepository
+  ) {}
+  async execute({
+    name,
+    email,
+    password,
+    departmentId,
+    role,
+  }: CreateUserParams): Promise<User> {
+      
+    const user = await this.userRepository.insertUser({
+      name,
+      email,
+      password,
+      role,
+      department_id: departmentId,
+    });
 
-        return user
-    }
+    return user;
+  }
 }
 export default CreateUserUseCase;
