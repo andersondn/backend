d6369a4d91e3273bd82e6c5ab6b837c4aaab5a91
Anderson Nunes <andersondngto@gmail.com>
2022-02-26 21:10:06 -0300

    feat: refistry department repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,14 @@
-import { container } from "tsyringe";
+import { container } from 'tsyringe';
+import DepartmentRepositoryKnex from '../../modules/department/infra/knex/DepartmentRepositoryKnex';
 import UserRepositoryKnex from '../../modules/user/infra/knex/UserRepositoryKnex';
-import UserRepository from "../../modules/user/repositories/UserRepository";
+import UserRepository from '../../modules/user/repositories/UserRepository';
 
 container.registerSingleton<UserRepository>(
     'UserRepository',
-    UserRepositoryKnex    
-)
+    UserRepositoryKnex
+);
+
+container.registerSingleton<DepartmentRepository>(
+    'DepartmentRepository',
+    DepartmentRepositoryKnex
+);
