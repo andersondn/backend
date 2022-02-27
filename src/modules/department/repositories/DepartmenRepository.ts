2c72792dc2a5e478b5b4a4ac5f265729f221eef6
Anderson Nunes <andersondngto@gmail.com>
2022-02-27 00:56:49 -0300

    feat: create update department method

--- src/modules/department/repositories/DepartmenRepository.ts
+++ src/modules/department/repositories/DepartmenRepository.ts
@@ -2,4 +2,7 @@ interface DepartmentRepository {
     createDepartment(department: Omit<Department, 'id' | 'created_at' | 'updated_at'>): Promise<Department>;
     listDepartments(): Promise<Department[]>;
     getDepartmentById(id: number): Promise<Department>;
+    updateDepartment(departmentId: number, department: Pick<Department, 'title' >): Promise<boolean>;
 }
+
+export default DepartmentRepository;
