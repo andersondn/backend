e378a706e3e76c12f79343a17d0f41d83376d19c
Anderson Nunes <andersondngto@gmail.com>
2022-02-26 21:58:28 -0300

    feat: create department repository

--- src/modules/department/infra/knex/DepartmentRepositoryKnex.ts
+++ src/modules/department/infra/knex/DepartmentRepositoryKnex.ts
@@ -1,7 +1,20 @@
-class DepartmentRepositoryKnex implements DepartmentRepository {
-    createDepartment(department: Omit<Department, "id" | "created_at" | "updated_at">): Promise<Department> {
-        throw new Error("Method not implemented.");
-    }
+import { knexConnection } from '../../../../shared/config/knexDB';
 
+class DepartmentRepositoryKnex implements DepartmentRepository {
+    async createDepartment(
+        department: Omit<Department, 'id' | 'created_at' | 'updated_at'>
+    ): Promise<Department> {
+        const [id] = await knexConnection('departments').insert({
+            ...department,
+            created_at: new Date(),
+            updated_at: new Date()
+        });
+        return {
+            id,
+            ...department,
+            created_at: new Date(),
+            updated_at: new Date()
+        };
+    }
 }
-export default DepartmentRepositoryKnex
+export default DepartmentRepositoryKnex;
