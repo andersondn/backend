2d4fd907669a442254f394e516f9d391fc130bbc
Anderson Nunes <andersondngto@gmail.com>
2022-02-26 22:14:28 -0300

    feat: list department

--- src/modules/department/repositories/DepartmenRepository.ts
+++ src/modules/department/repositories/DepartmenRepository.ts
@@ -1,4 +1,4 @@
 interface DepartmentRepository {
     createDepartment(department: Omit<Department, 'id' | 'created_at' | 'updated_at'>): Promise<Department>;
-
+    listDepartments(): Promise<Department[]>;
 }
