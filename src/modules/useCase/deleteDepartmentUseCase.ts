90e2c8dcb8a7bcbe67703aaafe054cd12d0e09f4
Anderson Nunes <andersondngto@gmail.com>
2022-02-27 01:10:47 -0300

    feat: create delete department use case

---
+++ src/modules/useCase/deleteDepartmentUseCase.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from 'tsyringe';
+import DepartmentRepository from '../department/repositories/DepartmentRepository';
+import AppError from '../../shared/helpers/AppError';
+
+@injectable()
+class DeleteDepartmentUseCase {
+    constructor(
+        @inject('DepartmentRepository')
+        private departmentRepository: DepartmentRepository
+    ) {}
+    async execute(departmentId: number): Promise<boolean> {
+        const checkDepartment =
+            await this.departmentRepository.getDepartmentById(departmentId);
+        if (!checkDepartment) {
+            throw new AppError({
+                message: 'Departamento não encontrado',
+                statusCode: 404
+            });
+        }
+
+        const department = await this.departmentRepository.deleteDepartment(
+            departmentId
+        );
+
+        return department;
+    }
+}
+export default DeleteDepartmentUseCase;
