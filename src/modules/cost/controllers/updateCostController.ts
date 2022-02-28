44e506e62cc1a2e7bcf1ac37e7f6c0cfb520732d
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 08:29:25 -0300

    feat: update cost controller

---
+++ src/modules/cost/controllers/updateCostController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+
+import {
+    Joi,
+    Segments,
+    validation
+} from '../../../shared/infra/http/express/middleware/Celebrate';
+import UpdateCostUseCase from '../useCase/updateCostUseCase';
+
+class UpdateCostController {
+    validate = validation({
+        [Segments.PARAMS]: Joi.object().keys({
+            costId: Joi.number().required()
+        }),
+        [Segments.BODY]: Joi.object().keys({
+            title: Joi.string().required(),
+            amount: Joi.number().required(),
+            date: Joi.date().required(),
+            user_id: Joi.number().required(),
+            department_id: Joi.number().required()
+        })
+    });
+    async handler(request: Request, response: Response) {
+        const { title, amount, date, user_id, department_id } = request.body;
+        const costId = request.params.costId as unknown as number;
+        const createCostUseCase = container.resolve(UpdateCostUseCase);
+        const result = await createCostUseCase.execute({
+            costId,
+            title,
+            amount,
+            date,
+            user_id,
+            department_id
+        });
+
+        if(!result) {
+            return response.status(500).json({
+                message: 'Falha ao atualizar custo'
+            });
+        }
+
+        return response.status(204).send();
+
+    }
+}
+
+
+
+export default UpdateCostController;
