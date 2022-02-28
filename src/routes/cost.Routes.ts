ff48e03173d0352eac11ec81740f733cd62ce99b
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 08:07:04 -0300

    feat: create route to list costs

--- src/routes/cost.Routes.ts
+++ src/routes/cost.Routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import CreateCostController from "../modules/cost/controllers/createCostController";
+import ListCostController from "../modules/cost/controllers/listCostsController";
 
 const routes = Router();
+const listCostController = new ListCostController();
 const createCostController = new CreateCostController();
+
+routes.get(
+    '/',
+    listCostController.handler
+);
 
 routes.post(
     '/',
