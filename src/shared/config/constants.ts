5eb1151abf867dd4d70fcd05f2568d81dea6a034
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 09:36:51 -0300

    config: create JWT secret

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -2,3 +2,4 @@ import 'dotenv/config'
 
 export const APP_PORT = process.env.PORT  || 4000
 export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const JWT_SECRET = process.env.JWT_SECRET || 'da39a3ee5e6b4b0d3255bfef95601890afd80709';
