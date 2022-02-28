d95b086553ffe6a756875df57d2955dc3da9bd66
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 09:37:11 -0300

    feat: create login use case

---
+++ src/modules/auth/useCase/loginUseCase.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from 'tsyringe';
+import AppError from '../../../shared/helpers/AppError';
+import UserRepository from '../../user/repositories/UserRepository';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../../../shared/config/constants';
+
+type LoginParams = {
+    email: string;
+    password: string;
+};
+
+@injectable()
+class LoginUseCase {
+    constructor(
+        @inject('UserRepository')
+        private userRepository: UserRepository
+    ) {}
+
+    async execute({ email, password }: LoginParams): Promise<string> {
+        const user = await this.userRepository.getUserByEmail(email);
+        if (user) {
+            if (await bcrypt.compare(password, user.password)) {
+                const token = jwt.sign(
+                    {
+                        id: user.id,
+                        email: user.email,
+                        name: user.name,
+                        role: user.role
+                    },
+                    JWT_SECRET,
+                    {
+                        expiresIn: '60m'
+                    }
+                );
+                return token;
+            }
+        }
+        throw new AppError({
+            message: 'Usuário e senha incorretos',
+            statusCode: 401
+        });
+    }
+}
+
+export default LoginUseCase;
