bd0614c8eb592959e81e57b5b368d02550f52564
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 10:00:26 -0300

    feat: secure routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,14 +3,15 @@ import userRoutes from './user.Routes'
 import departmentRoutes from './department.Routes'
 import costRoutes from './cost.Routes'
 import loginRoutes from './login.Routes'
+import authMiddleware from '../shared/infra/http/express/middleware/AuthMiddleware';
 
 const routes = Router();
 
 routes.get('/', (_, res) => res.send('Hello World'));
 
-routes.use('/users', userRoutes)
-routes.use('/departments', departmentRoutes)
-routes.use('/costs', costRoutes)
+routes.use('/users', authMiddleware, userRoutes)
+routes.use('/departments', authMiddleware, departmentRoutes)
+routes.use('/costs', authMiddleware, costRoutes)
 routes.use('/login', loginRoutes)
 
 export default routes
