0434ccfac8369fd8618387ae12a6469f55df8605
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 10:12:58 -0300

    feat: create route to sign up

--- src/routes/login.Routes.ts
+++ src/routes/login.Routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 import LoginController from '../modules/auth/controllers/loginController';
+import SignUpController from '../modules/auth/controllers/signUpController';
 
 const routes = Router();
 const loginController = new LoginController();
+const signUpController = new SignUpController();
 
 routes.post(
     '/',
     loginController.validate,
     loginController.handler
-    )
+)
+routes.post(
+    '/signUp',
+    signUpController.validate,
+    signUpController.handler
+)
 
 export default routes;
