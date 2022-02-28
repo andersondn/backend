116a18bea72437debd713d9e875b32c23a6b1386
Anderson Nunes <andersondngto@gmail.com>
2022-02-28 08:29:02 -0300

    feat: create route to update cost

--- src/routes/cost.Routes.ts
+++ src/routes/cost.Routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import CreateCostController from "../modules/cost/controllers/createCostController";
 import GetCostController from "../modules/cost/controllers/getCostController";
 import ListCostController from "../modules/cost/controllers/listCostsController";
+import UpdateCostController from '../modules/cost/controllers/updateCostController';
 
 const routes = Router();
 const listCostController = new ListCostController();
 const getCostController = new GetCostController();
 const createCostController = new CreateCostController();
-
+const updateCostController = new UpdateCostController();
 routes.get(
     '/',
     listCostController.handler
@@ -19,12 +20,19 @@ routes.get(
     getCostController.handler
 )
 
-
 routes.post(
     '/',
     createCostController.validate,
     createCostController.handler
 );
+
+routes.patch(
+    '/:costId',
+    updateCostController.validate,
+    updateCostController.handler
+);
+
+
     
 
 export default routes;
